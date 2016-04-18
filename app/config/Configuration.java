8657133fdcdeff48732a1446736d0a5d46068ea1
Nguyễn Phương <Nguyn Phng>
2016-04-18 22:54:33 +0700

    1

---
+++ app/config/Configuration.java
@@ -0,0 +1,8 @@
+package config;
+
+/**
+ * Created by Phuong on 18/04/2016.
+ */
+public class Configuration {
+    public static final String BASE_URL = "http://clothes:9000/";
+}
--- app/controllers/ClothesController.java
+++ app/controllers/ClothesController.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import models.Clothes;
 import play.mvc.Controller;
+
+import java.util.List;
 
 /**
  * Created by Phuong on 16/03/2016.
@@ -8,6 +11,8 @@ import play.mvc.Controller;
 public class ClothesController extends Controller {
 
     public static void homepage() {
+        List<Clothes> clothesList = Clothes.find("FROM Clothes").fetch(8);
+        renderArgs.put("clothesList", clothesList);
         render("clothes/homepage.html");
     }
 
